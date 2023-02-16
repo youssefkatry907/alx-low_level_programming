d1a7a809c3a177faa50abbc45d788d42d8ffbde5
youssefkatry907 <youssefkatry907@gmail.com>
2023-02-16 15:40:57 -0800

    last

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,15 @@
-/*
- *  * File: 101-quote.c
- *   * Auth: Brennan D Baraban
- **/
-
-#include <unistd.h>
+#include <stdio.h>
 
 /**
- *  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- *   *                followed by a new line, to standard error.
- *    *
- *     * Return: Always 1.
- * */
+ *  * main - Printing ouput to the standard error
+ *   *
+ *    * Return: 1 (Success)
+ *     * Using fprintf
+ **/
+
 int main(void)
 {
-write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);	      
+	fprintf(stderr,
+			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
 }
